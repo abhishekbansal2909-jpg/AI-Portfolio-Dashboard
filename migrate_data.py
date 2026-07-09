1e56bac060cda28225048277095c5ac1b0f50d91
abhishekbansal2909-jpg <abhishekbansal2909@gmail.com>
2026-07-09 16:40:12 +0530

    Create migrate_data.py

---
+++ migrate_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sqlite3
+
+CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSTUEBKe4VMr2NyAhlfw4uzeX2GIbbC8Tu_aUEGmHtpOxRmxE7Re_uxVu_0BB2vY_xcwwDWfRpmJkCV/pub?gid=0&single=true&output=csv"
+df = pd.read.csv(CSV_URL)
+
+df['Company Name'] = df['Company Name'].str.strip()
+df['Analysis Type'] = df['Analysis Type'].str.strip()
+
+conn = sqlite3.connect('portfolio.db')
+cursor = conn.cursor()
+
+for index, row in df.iterrows():
+  # question marks are acting as placeholders
+  cursor.execute('''
+    INSERT INTO assets (company_name, analysis_type, last_traded_price, ai_verdict, confidence_score)
+    VALUES (?,?,?,?,?) 
+  ''',(
+    row['Company Name'],
+    row['Analysis Type'],
+    row['Last Traded Price'],
+    row['AI Strategic Analysis'],
+    row['Confidence Score']
+
+  ))
+
+# Save the changes and lock the vault
+conn.commit()
+conn.close()
+
+print("Migration Complete and all spreadsheet data is locked in the vault")
