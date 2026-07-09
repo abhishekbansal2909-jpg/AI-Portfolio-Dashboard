5b03c660275ccc76eb50efae13a94c18c0074c0b
abhishekbansal2909-jpg <abhishekbansal2909@gmail.com>
2026-07-09 20:30:59 +0530

    Create api_test.py

---
+++ api_test.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import streamlit as st
+
+st.markdown("---")
+st.subheader("Live Api Test")
+
+try:
+  ticker = yf.Ticker("TATASTEEL.NS")
+  current_price = ticker.fast.info['last_price']
+
+  st.success(f"Success, Python directly pulled the live data for Tata Steel: ₹{round(current_price,2)}")
+
+except Exception as e:
+  st.error(f"API Connection failed: {e}")                              
