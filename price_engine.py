8d461e23eb6cba601166968a8114ad8cae2771cd
abhishekbansal2909-jpg <abhishekbansal2909@gmail.com>
2026-07-11 10:07:49 +0530

    Update price_engine.py

--- price_engine.py
+++ price_engine.py
@@ -1,37 +1,40 @@
 import yfinance as yf
 import time
-import sqlite3
 import pandas as pd
+import os
+import sqlalchemy
+from sqlalchemy import create_engine, text
 
 def update_prices():
   print("Initiating market sync...")
 
-  conn = sqlite3.connect('portfolio.db')
-  cursor = conn.cursor()
-  df = pd.read_sql_query("SELECT id, ticker FROM assets where ticker is NOT NULL", conn)
-  for index, row in df.iterrows():
-    ticker_symbol = row['ticker']
-
-    try:
-      ticker = yf.Ticker(ticker_symbol)
-      current_price = ticker.fast_info['last_price']
-
-      cursor.execute('''
-        UPDATE assets
-        SET last_traded_price = ?
-        WHERE id = ?
-      ''',(current_price, row['id']))
-
-      print(f"Updated {ticker_symbol} to ₹{round(current_price, 2)}")
-
-      time.sleep(1)
-
-    except Exception as e:
-      print(f" Failed to update {ticker_symbol}. Error:{e}")
-
-  conn.commit()
-  conn.close()
-  print("Market sync complete.")
+  db_url = os.environ.get("DATABASE_URL")
+  if db_url and db_url.startswith("postgres://):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+  engine = create_engine(db_url)
+  df = pd.read_sql_query("SELECT id, ticker FROM assets where ticker is NOT NULL", engine)
+  
+  with engine.begin() as conn:
+    for index, row in df.iterrows():
+      ticker_symbol = row['ticker']
+  
+      try:
+        ticker = yf.Ticker(ticker_symbol)
+        current_price = ticker.fast_info['last_price']
+  
+        query = text('''
+          UPDATE assets
+          SET last_traded_price = :price
+          WHERE id = :id
+        ''')
+        conn.execute(query, {"price": current_price, "id": row ['id']})
+        
+        print(f"Updated {ticker_symbol} to ₹{round(current_price, 2)}")
+  
+        time.sleep(1)
+  
+      except Exception as e:
+        print(f" Failed to update {ticker_symbol}. Error:{e}")
 
 if __name__ == "__main__":
   update_prices()
